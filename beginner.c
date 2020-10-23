f6516f891ec8095317fb41a726342ac363a33263
Nurdaulet <72253791+200109035@users.noreply.github.com>
2020-10-23 15:01:30 +0600

    start.c

---
+++ beginner.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main{
+
+ // bool isHasCar = true;
+ // int x = 0;
+ // while (isHasCar) {
+ //     printf("ведите цифру: ");
+ //     scanf("%d", &x);
+ //     if (x == 1)
+ //        isHasCar = false;
+
+    // float i = 1000;
+    // while(i > 10){
+    //     printf("%f\n", i);
+    //     i /= 20;
+
+    // for(int = x; x > 12; x++){
+    //     if(x == 4)
+    //     break;
+    //     printf("%d\n", x);  
+
+    //     if(x % 2 == 0)
+    //        continue;
+
+// bool isHasCar = false;
+// da {
+//     printf("Yes\n");
+// }while(isHasCar); 
+//   // (isHasCar) = false
+//   // (!isHasCar) = true
+ int arr[] = {5, 77, 8, 1, 17}
+ for(int i = 0; i < 5; i++){
+     printf("%d\n", arr[i]);
+
+   return 0;
+}
